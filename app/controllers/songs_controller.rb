849c7dcaaa46c2890ab512d8f52bcf4fb66512bb
Taybin Rutkin <taybin@taybin-rutkins-imac.local>
2009-04-02 19:51:44 -0400

    remove disallowed actions from the controller
    - regular users can't edit, delete, or view individual records.
    - edit the html for this.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,17 +10,6 @@ class SongsController < ApplicationController
     end
   end
 
-  # GET /songs/1
-  # GET /songs/1.xml
-  def show
-    @song = Song.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @song }
-    end
-  end
-
   # GET /songs/new
   # GET /songs/new.xml
   def new
@@ -30,11 +19,6 @@ class SongsController < ApplicationController
       format.html # new.html.erb
       format.xml  { render :xml => @song }
     end
-  end
-
-  # GET /songs/1/edit
-  def edit
-    @song = Song.find(params[:id])
   end
 
   # POST /songs
@@ -54,32 +38,4 @@ class SongsController < ApplicationController
     end
   end
 
-  # PUT /songs/1
-  # PUT /songs/1.xml
-  def update
-    @song = Song.find(params[:id])
-
-    respond_to do |format|
-      if @song.update_attributes(params[:song])
-        flash[:notice] = 'Song was successfully updated.'
-        format.html { redirect_to(@song) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @song.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /songs/1
-  # DELETE /songs/1.xml
-  def destroy
-    @song = Song.find(params[:id])
-    @song.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(songs_url) }
-      format.xml  { head :ok }
-    end
-  end
 end
