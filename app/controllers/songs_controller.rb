ec0f418c7d047a94a96db635cb284441acd183c9
Taybin Rutkin <taybin@taybin-rutkins-imac.local>
2009-04-02 19:55:31 -0400

    remove REST api
    - redirect to index after song creation

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,39 +1,32 @@
 class SongsController < ApplicationController
   # GET /songs
-  # GET /songs.xml
   def index
     @songs = Song.find(:all)
 
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @songs }
     end
   end
 
   # GET /songs/new
-  # GET /songs/new.xml
   def new
     @song = Song.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @song }
     end
   end
 
   # POST /songs
-  # POST /songs.xml
   def create
     @song = Song.new(params[:song])
 
     respond_to do |format|
       if @song.save
         flash[:notice] = 'Song was successfully created.'
-        format.html { redirect_to(@song) }
-        format.xml  { render :xml => @song, :status => :created, :location => @song }
+        format.html { redirect_to :action => "index" }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @song.errors, :status => :unprocessable_entity }
       end
     end
   end
